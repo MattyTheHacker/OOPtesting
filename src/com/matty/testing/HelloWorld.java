fe3fd76d8feab7b8302ca8dd935ae53c6b06c401
Matty Widdop <mrw078@cca-ug04-069.cs.bham.ac.uk>
2021-09-29 10:58:43 +0100

    Update 1

--- src/com/matty/testing/HelloWorld.java
+++ src/com/matty/testing/HelloWorld.java
@@ -1,7 +1,23 @@
 package com.matty.testing;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Formatter;
+
 public class HelloWorld {
-    public static void main(String[] args){
-        System.out.println("Hello World!");
+
+    public void print(LocalDate dateOfBirth, String name){
+
+        int age = Period.between(dateOfBirth, LocalDate.now()).getYears();
+
+        System.out.printf("Hello World! My name is %s", name);
+        System.out.printf("\nMy age is %d", age);
+    }
+
+    public static void main(String[] args) {
+        HelloWorld printer = new HelloWorld();
+        String myName = "Matthew";
+        LocalDate dateOfBirth = LocalDate.of(2002,3,25);
+        printer.print(dateOfBirth, myName);
     }
 }
