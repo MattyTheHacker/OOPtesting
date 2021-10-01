3df1b819627dd70d4d94678a0fc028d485ada830
BuildTools <unconfigured@null.spigotmc.org>
2021-10-01 19:17:23 +0100

    Add array testing

---
+++ src/com/matty/testing/Arrays.java
@@ -0,0 +1,16 @@
+package com.matty.testing;
+
+public class Arrays {
+    public static void main(String[] args) {
+        final int[] nums = new int[4];
+        nums[0] = 69;
+        nums[1] = 420;
+        nums[2] = 69420;
+        nums[3] = 666;
+        System.out.println(nums);
+        System.out.println(nums[0]);
+        System.out.println(nums[1]);
+        System.out.println(nums[2]);
+        System.out.println(nums[3]);
+    }
+}
