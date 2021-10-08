7365e4b8bffa651f8201e3ed5d64c95d218bd216
MattyTheHacker <matty258987@gmail.com>
2021-10-08 11:11:02 +0100

    add class creation template

---
+++ src/com/matty/testing/University.java
@@ -0,0 +1,13 @@
+package com.matty.testing;
+
+public class University {
+
+    public void setup() {
+
+    } // setup
+
+    public static void main(String[] args) {
+        University bham = new University();
+        bham.setup();
+    }//main
+}
