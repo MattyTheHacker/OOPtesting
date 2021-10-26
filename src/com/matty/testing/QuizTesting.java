594628a11fedb796f12b17eb97de2d727db9569c
MattyTheHacker <matty258987@gmail.com>
2021-10-26 21:22:42 +0100

    stuff and things

--- src/com/matty/testing/QuizTesting.java
+++ src/com/matty/testing/QuizTesting.java
@@ -3,13 +3,13 @@ package com.matty.testing;
 public class QuizTesting {
     public static void main(String[] args) {
         System.out.println("Begin Testing...");
-
-
         int x = 0;
         int s = 0;
-        int n = 10000;
-
+        int n = 1000;
         while (x < n){
+            System.out.println("x = " + x);
+            System.out.println("s = " + s);
+            System.out.println("==============");
             x++;
             s = s + 2 * x - 1;
             if (s == x * x){
