b59cc4f02653163e4934a68091226d596b5bd958
MattyTheHacker <matty258987@gmail.com>
2021-11-24 17:19:09 +0000

    mlfcs class test 2

---
+++ src/com/matty/testing/MathsAndLogic.java
@@ -0,0 +1,13 @@
+package com.matty.testing;
+
+public class MathsAndLogic {
+    public int doubleInt(int number) {
+        return number * 2;
+    }
+
+    public float addOneToFloat(float number) {
+        return number + 1.0f;
+    }
+
+
+}
