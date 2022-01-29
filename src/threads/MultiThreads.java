1b8a675be12395c1c07f0cc95db8ee0b9fea272c
Matty <matty258987@gmail.com>
2022-01-29 13:34:31 +0000

    finalise

--- src/threads/MultiThreads.java
+++ src/threads/MultiThreads.java
@@ -1,10 +1,11 @@
 package threads;
 
 public class MultiThreads {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ThreadExtender t1 = new ThreadExtender();
         Thread t2 = new Thread(new ThreadImplementer());
         t1.start();
+        t1.join(3000);
         t2.start();
     }
 }
--- src/threads/ThreadImplementer.java
+++ src/threads/ThreadImplementer.java
@@ -3,7 +3,7 @@ package threads;
 public class ThreadImplementer implements Runnable {
     @Override
     public void run() {
-        for (int i = 10; i > 0; i--) {
+        for (int i = 0; i < 10; i++) {
             System.out.println("[" + Thread.currentThread().getName() + "] " + i);
             try {
                 Thread.sleep(1000);
